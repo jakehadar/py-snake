347f46186756759b6c74cdbaa7b5ee8bdc38f3df
jake1025 <jake1025@gmail.com>
2020-01-12 12:50:37 -0500

    add tox

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 import io
+import os
 
 from pkg_resources import resource_string
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+MAJOR = 0
+MINOR = 0
+PATCH = 2
+
+VERSION = '{major}.{minor}.{patch}'.format(major=MAJOR, minor=MINOR, patch=PATCH)
+
+
+def parse_resource_string(filename):
+    text = None
+    try:
+        text = resource_string(__name__, filename).decode('utf-8')
+    except NotImplementedError:
+        # XXX: It is considered an anti-pattern to use __file__ or __path__ for locating package resource (despite its
+        # popularity in the dev community) because it breaks PEP 302-based import hooks, including when importing from
+        # zip files and Python Eggs. The correct pattern is to use pkg_resources.resource_string instead, however this
+        # seems to be incompatible with Python3...
+        here = os.path.abspath(os.path.dirname(__file__))
+        text = io.open(os.path.join(here, filename), encoding='utf-8').read()
+    finally:
+        return text
 
 
 setup(
@@ -15,7 +35,7 @@ setup(
     url='https://github.com/jakehadar/py-snake',
     python_requires='>=2.7',
     include_package_data=True,
-    long_description=resource_string(__name__, 'README.md').decode('utf-8'),
+    long_description=parse_resource_string('README.md'),
     long_description_content_type='text/markdown',
     keywords='snake game python cli command line',
     packages=find_packages(exclude=['tests', 'screenshots']),
@@ -31,8 +51,8 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
     ],
-    install_requires=resource_string(__name__, 'requirements.txt').decode('utf-8'),
-    license=resource_string(__name__, 'LICENSE.txt').decode('utf-8'),
+    install_requires=parse_resource_string('requirements.txt'),
+    license=parse_resource_string('LICENSE.txt'),
     tests_require=['pytest'],
     entry_points={
         'console_scripts': ['snake=snake.run:main']
